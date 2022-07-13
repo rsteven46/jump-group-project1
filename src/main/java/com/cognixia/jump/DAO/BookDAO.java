8ee0045243d90b548e8d474b7d2dafe9e243bd12
Nicholas Burns <42648266+nwburn3981@users.noreply.github.com>
2022-07-13 18:34:57 -0400

    Lots of menu progress, some book changes

--- src/main/java/com/cognixia/jump/DAO/BookDAO.java
+++ src/main/java/com/cognixia/jump/DAO/BookDAO.java
@@ -7,19 +7,15 @@ import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import com.cognixia.jump.ConnectionManager.ConnectionManager;
 import com.cognixia.jump.model.Book;
+import com.cognixia.jump.model.Tracker;
 
-
-
-public class BookDAO implements DAO<Book>{
+public class BookDAO implements DAO<Book> {
 
 	private Connection conn = ConnectionManager.getConnection();
-	
-	
-	
-	// List all books 
+
+	// List all books
 	@Override
 	public List<Book> findAll() {
 		List<Book> books = new ArrayList<>();
@@ -27,66 +23,102 @@ public class BookDAO implements DAO<Book>{
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String query = "";
-		
+
 		try {
 			query = "SELECT * from book";
 			pstmt = conn.prepareStatement(query);
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				
+
+			while (rs.next()) {
+
 				Book b = new Book();
 				b.setBookID(rs.getInt(1));
 				b.setName(rs.getString(2));
 				b.setAuthor(rs.getString(3));
 				b.setPages(rs.getInt(4));
-				
+
 				books.add(b);
-				
-				
+
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return books;
 	}
-	
-	
-	
+
+	public List<Book> findByName(List<Tracker> trackerList) {
+
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String query = "";
+		List<Book> bookList = new ArrayList<>();
+
+		Book book = null;
+		int bookID;
+
+		try {
+
+			query = "SELECT * FROM book WHERE bookID = ?";
+
+			pstmt = conn.prepareStatement(query);
+
+			for (Tracker tracker : trackerList) {
+				bookID = tracker.getBookID();
+				pstmt.setInt(1, bookID);
+
+				rs = pstmt.executeQuery();
+
+				while (rs.next()) {
+
+					book = new Book();
+					book.setBookID(rs.getInt(1));
+					book.setName(rs.getString(2));
+					book.setAuthor(rs.getString(3));
+					book.setPages(rs.getInt(4));
+
+					bookList.add(book);
+
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return bookList;
+	}
 
 	@Override
 	public boolean create(Book entity) {
-		//input a book into the database 
-		// 
-		
+		// input a book into the database
+		//
+
 		try {
-			String query = "Insert into book(bookID, bookName, author, pages)"
-					+ " values(?,?,?,?)";
-			
+			String query = "Insert into book(bookID, bookName, author, pages)" + " values(?,?,?,?)";
+
 			PreparedStatement pstmt = conn.prepareStatement(query);
-			
+
 			pstmt.setNull(1, Types.INTEGER);
 			pstmt.setString(2, entity.getName());
 			pstmt.setString(3, entity.getAuthor());
 			pstmt.setInt(4, entity.getPages());
-			
-			int numInserts =pstmt.executeUpdate();
-			
-			if(numInserts >0) {
-				System.out.println("Entity " +entity +" added to db.");
+
+			int numInserts = pstmt.executeUpdate();
+
+			if (numInserts > 0) {
+				System.out.println("Entity " + entity + " added to db.");
 				return true;
 			}
-			
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		return false;
-		
+
 	}
 
 	@Override
--- src/main/java/com/cognixia/jump/ProgressDriver.java
+++ src/main/java/com/cognixia/jump/ProgressDriver.java
@@ -3,12 +3,17 @@ package com.cognixia.jump;
 import java.util.Scanner;
 
 import com.cognixia.jump.DAO.UserDAO;
+import com.cognixia.jump.menu.ConsoleUserInterface;
 import com.cognixia.jump.model.User;
 
 public class ProgressDriver {
 
 	public static void main(String[] args) {
 
+		Scanner scan = new Scanner(System.in);
+		
+		
+		ConsoleUserInterface.userPrompt(scan);
 	}
 
 }
