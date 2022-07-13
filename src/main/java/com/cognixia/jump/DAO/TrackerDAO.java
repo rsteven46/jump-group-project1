176e890bb6d8653ebc802e4f77bc4f806de94d00
Steven Ryan Ramos <43050949+rsteven46@users.noreply.github.com>
2022-07-13 11:39:26 -0700

    Updated Dao for more generic use

--- src/main/java/com/cognixia/jump/DAO/TrackerDAO.java
+++ src/main/java/com/cognixia/jump/DAO/TrackerDAO.java
@@ -7,33 +7,29 @@ import com.cognixia.jump.model.Tracker;
 public class TrackerDAO implements DAO<Tracker> {
 
 	@Override
-	public Tracker findById(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Tracker findByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public List<Tracker> findAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public boolean addStatus(Tracker entity) {
+	public boolean create(Tracker entity) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean updateStatus(Tracker entity) {
+	public boolean remove(Tracker entity) {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
+	@Override
+	public boolean update(Tracker entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
 
 }
