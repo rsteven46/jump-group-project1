913123494815af0b4f5050df8703658a45b8e70c
Zainal Shariff <shariff.zainal7@gmail.com>
2022-07-13 09:55:20 -0700

    uploaded structure

---
+++ src/main/java/com/cognixia/jump/DAO/BookDAO.java
@@ -0,0 +1,39 @@
+package com.cognixia.jump.DAO;
+
+import java.util.List;
+
+import com.cognixia.jump.model.Book;
+
+public class BookDAO implements DAO<Book>{
+
+	@Override
+	public Book findById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Book findByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Book> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addStatus(Book entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateStatus(Book entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/main/java/com/cognixia/jump/DAO/UserDAO.java
@@ -0,0 +1,39 @@
+package com.cognixia.jump.DAO;
+
+import java.util.List;
+
+import com.cognixia.jump.model.User;
+
+public class UserDAO implements DAO<User>{
+
+	@Override
+	public User findById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addStatus(User entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateStatus(User entity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
